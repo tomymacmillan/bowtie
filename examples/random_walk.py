70f20078f5af33e8d8d937673ccca02758ea3e7c
Jacques Kvam <jwkvam@gmail.com>
2017-04-27 12:54:57 -0700

    update random walk so it can be used with multiple users

--- examples/random_walk.py
+++ examples/random_walk.py
@@ -7,31 +7,44 @@ Example Bowtie App.
 from bowtie.control import Nouislider
 from bowtie.visual import Plotly
 from bowtie import command
+from bowtie import Pager
+from bowtie import cache
 
 import numpy as np
 from numpy import random as rng
 import plotlywrapper as pw
 
 
-sigma = Nouislider(caption='Sigma', start=1, minimum=0.1, maximum=50)
+pager = Pager()
+sigma = Nouislider(caption='Sigma', start=0., minimum=0.1, maximum=50.)
 mainplot = Plotly()
 
-data = np.zeros(100).tolist()
+
+def initialize():
+    cache.save('data', [0.] * 100)
 
 
-def walk():
+def upgraph():
+    data = cache.load('data')
     value = float(sigma.get())
     data.pop(0)
     data.append(value * rng.randn() + data[-1])
     mainplot.do_all(pw.line(data).to_json())
+    cache.save('data', data)
+
+
+def walk():
+    pager.notify()
 
 
 @command
 def construct():
     from bowtie import Layout
-    layout = Layout(debug=True)
+    layout = Layout(debug=False)
     layout.add_sidebar(sigma)
     layout.add(mainplot)
+    layout.load(initialize)
     layout.schedule(0.1, walk)
+    layout.respond(pager, upgraph)
 
     layout.build()
